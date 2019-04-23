82c3f4662dbc330d904d9fe0499ef381013d78d6
Kim Thompson <kthompson@mpr.org>
2019-04-23 10:54:24 -0500

    fixed generateSrcSet if aspectRatio doesn't exist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Image } from './lib';
-import { image } from './lib/Image/testdata/image';
+// import { image } from './lib/Image/testdata/image';
+import gif from './lib/Image/testdata/gif.json';
 
 const App = () => (
   <div>
     <h1>Hello React</h1>
     <Image
-      image={image}
+      image={gif}
       aspectRatio="square"
       alt="this image is from our test data"
       elementClass="exampleImage"
