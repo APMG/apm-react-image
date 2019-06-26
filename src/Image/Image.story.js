33c39ef846536d6f62472d124d3dfcdf3ea85d30
Kim Thompson <kthompson@mpr.org>
2019-06-26 11:39:14 -0500

    added stories, switched to rollup

---
+++ src/Image/Image.story.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Image from './Image';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { withReadme } from 'storybook-readme';
+import readme from './README.md';
+import { image } from './testdata/image';
+
+const stories = storiesOf('Atoms/Figure', module);
+
+stories.addDecorator(withKnobs()).addDecorator(withReadme([readme]));
+
+stories.add('Image Endpoint', () => {
+  return <Image image={image} />;
+});
+
+stories.add('Aspect Ratio Square', () => {
+  return <Image image={image} aspectRatio="square" />;
+});
+
+stories.add('Fallback', () => {
+  return (
+    <Image
+      fallbackSrc={text('Example Image Src', 'http://placecorgi.com/800')}
+      alt={text('Image Alt Text', 'a good boi')}
+    />
+  );
+});
+
+stories.add('');
